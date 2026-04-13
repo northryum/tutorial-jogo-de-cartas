8dc9f69d493df4596da8e0e63cc09d95c8f90bff
northryum <northryum@gmail.com>
2026-04-13 18:32:56 -0400

    EP1 - DRAGGING CARDS
    feito o ep 1

---
+++ Scripts/Card_Manager.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+var COLISION_MASK_CARD = 1
+
+var card_being_dragged
+var screen_size
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+
+func _process(_delta: float) -> void:
+	if card_being_dragged:
+		var mouse_pos = get_global_mouse_position()
+		card_being_dragged.position = Vector2(clamp(mouse_pos.x, 0, screen_size.x),
+		 clamp(mouse_pos.y, 0, screen_size.y))
+	pass
+
+func _input(_event):
+	if _event is InputEventMouseButton and _event.button_index == MOUSE_BUTTON_LEFT:
+		if _event.is_pressed():
+			var card = _rayCast_check_for_card()
+			if card:
+				card_being_dragged = card
+		else:
+			card_being_dragged = null
+	pass
+
+func _rayCast_check_for_card():
+	var space_state = get_world_2d().direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = get_global_mouse_position()
+	parameters.collide_with_areas = true
+	parameters.collision_mask = COLISION_MASK_CARD
+	var result = space_state.intersect_point(parameters)
+	if result.size() > 0:
+		return result[0].collider.get_parent()
+	return null
